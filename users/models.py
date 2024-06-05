163db8b228f96d89cacabde05654973cc0fbce88
bahromnajmiddinov <najmiddinovbahrom402@gmail.com>
2024-06-05 09:31:02 +0500

    first commit

---
+++ users/models.py
@@ -0,0 +1,28 @@
+import os
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class GenerateProfileImagePath(object):
+    def __init__(self):
+        pass
+    
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        path = f'media/accounts/{instance.id}/images/'
+        name = f'profile_image.{ext}'
+        return os.path.join(path, name)
+
+
+user_profile_image_path = GenerateProfileImagePath()
+
+
+class Profile(AbstractUser):
+    image = models.FileField(upload_to=user_profile_image_path, null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.username}'
+        
